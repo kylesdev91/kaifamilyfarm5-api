4806377841171d412b0f420d17d5b262b95f4904
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-07 17:21:03 -0500

    added mailOption

--- sendemail/index.js
+++ sendemail/index.js
@@ -7,24 +7,26 @@ module.exports = async function (context, req) {
   const vaultName = 'kaifamilyfarm5KV';
   const url = `https://${vaultName}.vault.azure.net`;
   const client = new SecretClient(url, credential);
-  const userRetrievedSecret = await client.getSecret('username2');
-  const username2 = userRetrievedSecret.value;
-  const pwdRetrievedSecret = await client.getSecret('password2');
-  const password2 = pwdRetrievedSecret.value;
+  const userRetrievedSecret = await client.getSecret('username1');
+  const username1 = userRetrievedSecret.value;
+  const pwdRetrievedSecret = await client.getSecret('password1');
+  const password1 = pwdRetrievedSecret.value;
 
   let transporter = nodemailer.createTransport({
     service: 'hotmail',
     auth: {
-      user: username2,
-      pass: password2,
+      user: username1,
+      pass: password1,
     },
   });
 
   const mailOptions = {
-    from: 'kaifamilyfarm@yahoo.com',
+    from: 'kylehernandez12@outlook.com',
+    // to: 'eands9@yahoo.com',
+    // from: 'kaifamilyfarm@yahoo.com',
     to: 'kaifamilyfarm@yahoo.com',
-    // from: 'kffsande123456@outlook.com',
-    // to: 'kffsande123456@outlook.com',
+    // from: 'kylehernandez12@outlook.com',
+    // to: 'kylehernandez12@outlook.com',
     subject:
       'Order From ' + req.body.emailAddress + ' - ' + req.body.emailSubject,
     text: req.body.emailBody,
