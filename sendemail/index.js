d04bfefa76c75dc62f83d80c974aead7540e4d88
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-06 22:16:52 -0500

    added email

--- sendemail/index.js
+++ sendemail/index.js
@@ -21,8 +21,8 @@ module.exports = async function (context, req) {
   });
 
   const mailOptions = {
-    from: 'kffsande12345@outlook.com',
-    to: 'kffsande12345@outlook.com',
+    from: 'kaifamilyfarm@yahoo.com',
+    to: 'kaifamilyfarm@yahoo.com',
     subject:
       'Order From ' + req.body.emailAddress + ' - ' + req.body.emailSubject,
     text: req.body.emailBody,
