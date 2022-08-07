87370b4df4719133b6d4729ffc5389817cebcbb9
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-07 13:14:21 -0500

    change back password

--- sendemail/index.js
+++ sendemail/index.js
@@ -21,8 +21,10 @@ module.exports = async function (context, req) {
   });
 
   const mailOptions = {
-    from: 'kaifamilyfarm@yahoo.com',
-    to: 'kaifamilyfarm@yahoo.com',
+    // from: 'kaifamilyfarm@yahoo.com',
+    // to: 'kaifamilyfarm@yahoo.com',
+    from: 'kffsande12345@outlook.com',
+    to: 'kffsande12345@outlook.com',
     subject:
       'Order From ' + req.body.emailAddress + ' - ' + req.body.emailSubject,
     text: req.body.emailBody,
